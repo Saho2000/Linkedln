f3abdada9c1cfc277c3d6b3c8af85673d9e5c6df
Sahosrabdi <sahosrabdidas.2016@gmail.com>
2026-04-13 00:37:42 +0530

    setup of kafka messaging along with notifications

---
+++ notification-service/src/main/java/com/linkedln/post_service/event/PostCreatedEvent.java
@@ -0,0 +1,11 @@
+package com.linkedln.post_service.event;
+
+import lombok.Builder;
+import lombok.Data;
+
+@Data
+public class PostCreatedEvent {
+    Long creatorId;
+    String content;
+    Long postId;
+}
---
+++ notification-service/src/main/java/com/linkedln/post_service/event/PostLikedEvent.java
@@ -0,0 +1,11 @@
+package com.linkedln.post_service.event;
+
+import lombok.Builder;
+import lombok.Data;
+
+@Data
+public class PostLikedEvent {
+    Long postId;
+    Long creatorId;
+    Long likedByUserId;
+}
